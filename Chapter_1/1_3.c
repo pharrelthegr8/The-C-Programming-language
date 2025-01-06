b25306de89e7d564c7151a0c0791fe9c8a6b6413
Pharrel.O <pharrelthegr8@gmail.com>
2025-01-06 17:58:21 +0300

    Add 1_3

--- Chapter_1/1_3.c
+++ Chapter_1/1_3.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LOWER 0
-#define UPPER 300
-#define INCREMENT 20
+// symbolic constants
+#define LOWER 0 // lowest limit
+#define UPPER 300 // highest limit
+#define INCREMENT 20 // increment size
 
 int main(){
   
-  system("clear");
-  printf("Temperature Converter\n");
+  system("clear"); // just to clear the terminal every time the code runs
+  printf("Temperature Converter\n"); // the heading for the table
   printf("\n");
   int fahr;
 
