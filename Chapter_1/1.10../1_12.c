6282c9c2feaa9fd69a4de14a3cfae48383216c7a
Pharrel.O <pharrelthegr8@gmail.com>
2025-01-07 16:38:55 +0300

    add 1_9

---
+++ Chapter_1/1.10../1_12.c
@@ -0,0 +1,24 @@
+// a program that prints its input one word per line
+
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(){
+
+  int character;
+  int state = OUT;
+
+  while((character = getchar()) != EOF){
+
+    if(character != ' ' && character != '\t'){
+      state = IN;
+      putchar(character);
+
+    }else if(state == IN){
+      printf("\n");
+      state = OUT;
+    }
+  }
+}
--- Chapter_1/1_9.c
+++ Chapter_1/1_9.c
@@ -2,26 +2,18 @@
 // replacing each string of one or more blanks by a single blank
 #include <stdio.h>
 
-#define YES 1
-#define NO 0
-
 int main(){
 
-  int character, was_it_a_blank;
-  was_it_a_blank = NO; // track blanks
+   int character, previous;
 
-  while((character = getchar()) != EOF){
-    if(character == ' '){
-      if(was_it_a_blank == NO){
-        // zero means there was no blank
-        putchar(character); // print first blank
-        was_it_a_blank = YES; // set to 1 since a blank has been encountered
-        // when was_it_a_blank is set to 1 all other blanks encountered afterwards are not printed
-      }
-    }else{
-        // if the character is not a blank print it
-        was_it_a_blank = NO; // reset value
-        putchar(character);
-    }
-  } 
+   while((character = getchar()) != EOF){
+     if(character != ' ' || previous != ' '){
+       
+       putchar(character);
+
+     }
+     previous = character;
+   }
 }
+// This keeps only one variable ahead of the other and when a space is encountered
+// only the first space is printed the rest are ignored
