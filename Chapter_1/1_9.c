847a93bbd41645163fd3c290d42292cadc4baee9
Pharrel.O <pharrelthegr8@gmail.com>
2025-01-07 11:24:13 +0300

    Add 1_9

---
+++ Chapter_1/1_9.c
@@ -0,0 +1,27 @@
+// a program to copy its input to its output, 
+// replacing each string of one or more blanks by a single blank
+#include <stdio.h>
+
+#define YES 1
+#define NO 0
+
+int main(){
+
+  int character, was_it_a_blank;
+  was_it_a_blank = 0; // track blanks
+
+  while((character = getchar()) != EOF){
+    if(character == ' '){
+      if(was_it_a_blank == 0){
+        // zero means there was no blank
+        putchar(character); // print first blank
+        was_it_a_blank = 1; // set to 1 since a blank has been encountered
+        // when was_it_a_blank is set to 1 all other blanks encountered afterwards are not printed
+      }
+    }else{
+        // if the character is not a blank print it
+        was_it_a_blank = 0; // reset value
+        putchar(character);
+    }
+  } 
+}
